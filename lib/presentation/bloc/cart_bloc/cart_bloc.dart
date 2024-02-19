9fbf13ee8b8b9f24a110b77d9aa0f1bf594351bf
shaun hossain <shaunhossain95@gmail.com>
2024-02-20 00:04:01 +0600

    - add a stream from real time database watch

--- lib/presentation/bloc/cart_bloc/cart_bloc.dart
+++ lib/presentation/bloc/cart_bloc/cart_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
--- lib/presentation/ui/pages/cart_page.dart
+++ lib/presentation/ui/pages/cart_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tr_store/data/database/helper/db_instance.dart';
 import 'package:tr_store/domain/product_response/product_response.dart';
 import 'package:tr_store/presentation/bloc/cart_bloc/cart_bloc.dart';
 import 'package:tr_store/presentation/ui/widgets/cart_item.dart';
@@ -14,17 +17,36 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   int productCount = 1;
+  StreamController<List<ProductResponse>> controller =
+      StreamController<List<ProductResponse>>();
 
   @override
   void initState() {
     context.read<CartBloc>().add(const CartEvent.productList());
+    controller.addStream(productDatabase
+        .select(productDatabase.product)
+        .map((item) => ProductResponse(
+              id: item.id,
+              slug: item.slug,
+              title: item.title,
+              content: item.content,
+              image: item.image,
+              thumbnail: item.thumbnail,
+              status: item.status,
+              category: item.category,
+              publishedAt: item.publishedAt,
+              updatedAt: item.updatedAt,
+              userId: item.userId,
+              count: item.count,
+            ))
+        .watch());
     super.initState();
   }
 
   void add({required ProductResponse productResponse}) {
     productCount = productResponse.count ?? 0;
     setState(() {
-      if (productCount < 10) {
+      if (productCount < 100) {
         productCount = productCount + 1;
       }
     });
@@ -49,38 +71,37 @@ class _CartPageState extends State<CartPage> {
       appBar: AppBar(
         title: const Text("Add Cart"),
       ),
-      body: BlocBuilder<CartBloc, CartState>(builder: (context, state) {
-        if (state.status == CartStatus.success) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: CustomScrollView(
-              slivers: [
-                SliverList.builder(
-                    itemCount: state.listOfProducts?.length,
-                    itemBuilder: (context, index) {
-                      return CartItem(
-                        item: state.listOfProducts?[index],
-                        deleteItem: () {
-                          context.read<CartBloc>().add(CartEvent.deleteCart(
-                              productResponse: state.listOfProducts![index]));
-                        },
-                        minusItem: (){
-                          remove(productResponse: state.listOfProducts![index]);
-                        },
-                        addItem: () {
-                          add(productResponse: state.listOfProducts![index]);
-                        },
-                      );
-                    }),
-                  SliverToBoxAdapter(
-                    child: CustomMyCartFooter(checkOut: (){}, price: 0),
-                  )
-                ],
-            ),
-          );
-        }
-        return const SizedBox();
-      }),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: CustomScrollView(
+          slivers: [
+            StreamBuilder(
+                stream: controller.stream,
+                builder: (context, data) {
+                  return SliverList.builder(
+                      itemCount: data.data?.length,
+                      itemBuilder: (context, index) {
+                        return CartItem(
+                          item: data.data?[index],
+                          deleteItem: () {
+                            context.read<CartBloc>().add(CartEvent.deleteCart(
+                                productResponse: data.data![index]));
+                          },
+                          minusItem: () {
+                            remove(productResponse: data.data![index]);
+                          },
+                          addItem: () {
+                            add(productResponse: data.data![index]);
+                          },
+                        );
+                      });
+                }),
+            SliverToBoxAdapter(
+              child: CustomMyCartFooter(checkOut: () {}, price: 0),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
